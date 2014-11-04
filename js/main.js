764d3c37d6aa3472f30b709ce742a329a596b884
keren kilunovsky <kerenk15@gmail.com>
2014-11-05 01:29:57 +0200

    ajax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+window.onload = UTILS.ajax('../js/notification.txt', {
+				method:'GET',
+				done: function(response){
+						console.log(response);
+						var words = response.split(" ");
+						var i , p = '<p>';
+						for (i=0; i<words.length; i++) {
+							if (words[i]){
+								p += words[i] + ' ';
+							}
+						}
+
+						p += '<p>';
+						if(words.length <= 1){
+							document.querySelector('#notifications').style.display = 'none';
+							}
+						else{
+							document.querySelector('#notifications').innerHTML = p;
+						}
+					}
+			});
