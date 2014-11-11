9f0def5ec01df12b0a213f63d9d97d6dc61b77eb
keren kilunovsky <kerenk15@gmail.com>
2014-11-12 00:26:46 +0200

    tabs

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-
 window.onload = UTILS.ajax('../js/notification.txt', {
 				method:'GET',
 				done: function(response){
 						console.log(response);
-						var words = response.split(" ");
-						var i , p = '<p>';
-						for (i=0; i<words.length; i++) {
-							if (words[i]){
-								p += words[i] + ' ';
-							}
-						}
+						var i , p = '<p>' + response + '</p>';
+						console.log(p.length);
 
-						p += '<p>';
-						if(words.length <= 1){
+						if(response.length <= 1){
 							document.querySelector('#notifications').style.display = 'none';
 							}
 						else{
@@ -20,3 +13,42 @@ window.onload = UTILS.ajax('../js/notification.txt', {
 						}
 					}
 			});
+
+var funcRef = function(){
+var hash = location.hash.slice(1),
+	tabs = document.querySelectorAll('.tabs > div'),
+	link = document.querySelector('.tabs > ul > li > a');
+
+if (hash === '') {
+	hash = link.href.split('#')[1];
+}
+
+for (var i = 0; i < tabs.length; i++) {
+	tabs[i].className = 'hidden';
+}
+
+document.querySelector('#pre-fix-' + hash).className = '';
+
+};
+
+
+
+/*if (hash === '') {
+	hash = link.href.split('#')[1];
+}
+
+for (var i = 0; i < tabs.length; i++) {
+	UTILS.className(tabs[i] , hidden);
+}
+
+UTILS.className(document.querySelector('#' + hash) , '');
+};*/
+
+/*var funcRef = function(){
+var hash = location.hash.slice(1),
+	tabs= document.querySelectorAll('.tabs > div');
+for (var i = 0; i < tabs.length; i++) {
+	tabs[i].className = 'hidden';
+}
+document.querySelector('#' + hash).className = '';
+};*/
