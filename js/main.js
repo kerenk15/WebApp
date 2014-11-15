61b68e1339e98f2ce0874ceaf499659df9dc8e5e
keren kilunovsky <kerenk15@gmail.com>
2014-11-15 19:58:54 +0200

    tabs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-window.onload = UTILS.ajax('../js/notification.txt', {
+window.onload = (function() {
+
+	UTILS.ajax('../js/notification.txt', {
 				method:'GET',
 				done: function(response){
 						console.log(response);
@@ -14,10 +16,37 @@ window.onload = UTILS.ajax('../js/notification.txt', {
 					}
 			});
 
+
+var settings = document.querySelectorAll('.settings');
+console.log(settings);
+
+for (var i = 0; i < settings.length; i++) {
+	settings[i].addEventListener('click',checkClass);
+}
+
+var checkClass =  function(e){
+	console.log('setting clicked');
+	console.log(settings[i].className);
+	/* = className + hidden;e.preventDefault();*/
+
+
+
+
+
+
+
+
+};
+
+
+})();
+
+
 var funcRef = function(){
 var hash = location.hash.slice(1),
 	tabs = document.querySelectorAll('.tabs > div'),
-	link = document.querySelector('.tabs > ul > li > a');
+	link = document.querySelector('.tabs > ul > li > a'),
+	Activelink = document.querySelectorAll('.tabs > ul > li > a');
 
 if (hash === '') {
 	hash = link.href.split('#')[1];
@@ -25,18 +54,25 @@ if (hash === '') {
 
 for (var i = 0; i < tabs.length; i++) {
 	tabs[i].className = 'hidden';
+	Activelink[i].parentNode.id = '';
+	if (Activelink[i].href.split('#')[1] === hash)
+		Activelink[i].parentNode.id = 'active';
 }
 
 document.querySelector('#pre-fix-' + hash).className = '';
-
 };
 
+/*var tabs_list = document.querySelectorAll('.tabs > ul > li > a');
+console.log(tabs_list);
+tabs_list.addEventListener('keyup', function(){
+console.log(e.keyCode);
+	if (e.keyCode === 13 || e.keyCode === 32){
+		e.preventDefault();
+		funcRef();
+	}
+});*/
 
-
-/*if (hash === '') {
-	hash = link.href.split('#')[1];
-}
-
+/*
 for (var i = 0; i < tabs.length; i++) {
 	UTILS.className(tabs[i] , hidden);
 }
@@ -44,11 +80,22 @@ for (var i = 0; i < tabs.length; i++) {
 UTILS.className(document.querySelector('#' + hash) , '');
 };*/
 
-/*var funcRef = function(){
-var hash = location.hash.slice(1),
-	tabs= document.querySelectorAll('.tabs > div');
-for (var i = 0; i < tabs.length; i++) {
-	tabs[i].className = 'hidden';
+
+
+var expand = document.querySelectorAll('.expand');
+console.log(expand);
+for (var j= 0; j < expand.length; j++) {
+	expand[j].addEventListener('click',openURL);
 }
-document.querySelector('#' + hash).className = '';
-};*/
+
+var openURL = function (e){
+	console.log(expand[j]);
+var target = e.target,
+	url = terget.href,
+	iframe = window.frames['#ynet'];
+e.preventDefault();
+target = ('_blank');
+
+url = iframe.src;
+
+};
